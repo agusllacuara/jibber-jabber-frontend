a04d004289be13cb4a9bc2da6ff7b3b9e22feb9a
nicolefox <fox_nicole@hotmail.com>
2021-07-06 18:39:00 -0300

    url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   name: 'production',
-  chatUrl: 'http://jibbyjabber.hopto.org:8084',
-  backend: 'http://jibbyjabber.hopto.org:8080'
+  chatUrl: 'http://jibby-jabber-messages:8084',
+  backend: 'http://jibby-jabber:8080'
 };
