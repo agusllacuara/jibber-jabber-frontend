a0abcf72381e29e2a87fc3e3417c13269d576e32
nicolefox <fox_nicole@hotmail.com>
2021-07-06 18:57:07 -0300

    environment url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   name: 'production',
-  chatUrl: 'http://jibby-jabber-messages:8084',
-  backend: 'http://jibby-jabber:8080'
+  chatUrl: '/messages',
+  backend: '/api'
 };
